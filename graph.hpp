66a3ea1f9cab02b9d3276b644b3d1e4d31758e6d
wojciechaniol <162443006+wojciechaniol@users.noreply.github.com>
2024-12-19 19:25:02 +0100

    Update graph.hpp

--- graph.hpp
+++ graph.hpp
@@ -5,29 +5,27 @@
 #include <algorithm>
 #include <random>
 #include <queue>
+#include <limits.h>
+#include <chrono>
 
 using namespace std;
 
-random_device rd;
-mt19937 engine(rd());
-
 class Graph
 {
-    int** adjacency_matrix;
-    int** sequence_array;
-    int chromatic_number;
-    int vertices;
+    private:
+        int** adjacency_matrix;
+        int vertices;
 
     public:
 
         Graph(int n);
         Graph* clone();
-        void generate_graph(int saturation);
+        void generate_graph(double saturation);
         void check_bridges();
         bool active_vertex(int v);
         bool is_clique();
         void merge_vertices(int u, int v);
-        void add_edge(int u, int v, bool sequence=true);
+        void add_edge(int u, int v);
         void printing();
         void coloring_print(int* chromatic_value);
         void exhaustive_search();
@@ -35,17 +33,17 @@ class Graph
         bool is_safe(int* colors, int node, int color);
         void backtracking(int* colors, int node, int numColors);
         void welsh_powell();
-        void partition(int** color_array);
+        void partition(int** color_array, int available_colors);
         void palette_restriciton(int available_colors);
         int zykov(Graph& graph);
         int do_zykov();
-        int countVertices();
+        int count_vertices();
         void genetic(int population_size);
         void create_individual(int* colors, int max_color);
         int calculate_penalty(int* individual);
         void crossover(int* parent_1, int* parent_2);
         void mutation(int* individual, int max_color, double probability);
-        int** selection(int** population, int population_size)
+        int** selection(int** population, int population_size);
 };
 
 #endif
